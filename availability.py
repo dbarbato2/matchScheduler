81dbf2c8a77741a42bb63fde0b570c96f45b10bf
dbarbato2 <dbarbato2@gmail.com>
2023-10-15 16:04:27 -0400

    Update availability.py

--- availability.py
+++ availability.py
@@ -7,19 +7,37 @@ st.title("Availability Scheduler")
 # Create a form to collect user availability data
 st.write("Check which times you are available for each day of the week:")
 
-days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']# Define the start and end times
+days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+                'Sunday']  # Define the start and end times
 colMonday, colTuesday, colWednesday, colThursday, colFriday, colSaturday, colSunday = st.columns(7)
-dayCols = []
-for day in days_of_week:
-	dayCols.append("col" + day)
-st.write(dayCols[0])
 
 openTime = 8  # 8 AM
 closeTime = 21  # 9 PM
 
 availability_data = {}
 for day in days_of_week:
-    for hour in range(openTime, closeTime+1):
+    if day == "Monday":
+        with colMonday:
+            st.subheader("Monday")
+    elif day == "Tuesday":
+        with colTuesday:
+            st.subheader("Tuesday")
+    elif day == "Wednesday":
+        with colWednesday:
+            st.subheader("Wednesday")
+    elif day == "Thursday":
+        with colThursday:
+            st.subheader("Thursday")
+    elif day == "Friday":
+        with colFriday:
+            st.subheader("Friday")
+    elif day == "Saturday":
+        with colSaturday:
+            st.subheader("Saturday")
+    else:
+        with colSunday:
+            st.subheader("Sunday")
+    for hour in range(openTime, closeTime + 1):
         # Convert the hour to 12-hour format and determine AM or PM
         if hour < 12:
             am_pm = "AM"
@@ -30,8 +48,27 @@ for day in days_of_week:
             if hour > 12:
                 hour -= 12  # Convert to 1-based 12-hour clock
         time_str = f"{hour:02d}:00 {am_pm}"
-        with dayCols[0]:
-            availStr = st.checkbox(day + ", " + time_str)
+        if day == "Monday":
+            with colMonday:
+                availStr = st.checkbox(time_str, key=day + ", " + time_str)
+        elif day == "Tuesday":
+            with colTuesday:
+                availStr = st.checkbox(time_str, key=day + ", " + time_str)
+        elif day == "Wednesday":
+            with colWednesday:
+                availStr = st.checkbox(time_str, key=day + ", " + time_str)
+        elif day == "Thursday":
+            with colThursday:
+                availStr = st.checkbox(time_str, key=day + ", " + time_str)
+        elif day == "Friday":
+            with colFriday:
+                availStr = st.checkbox(time_str, key=day + ", " + time_str)
+        elif day == "Saturday":
+            with colSaturday:
+                availStr = st.checkbox(time_str, key=day + ", " + time_str)
+        else:
+            with colSunday:
+                availStr = st.checkbox(time_str, key=day + ", " + time_str)
     availability_data[day, hour] = availStr
 
 # Create a submit button to save availability data to a CSV file
@@ -46,6 +83,7 @@ if st.button("Submit"):
 
 # Provide a download link for the generated CSV file
 st.write("Download your availability:")
-st.markdown("[Download user_availability.csv](data:file/csv;base64, " + availability_df.to_csv().encode('utf-8').decode('utf-8') + ")")
+st.markdown("[Download user_availability.csv](data:file/csv;base64, " + availability_df.to_csv().encode('utf-8').decode(
+    'utf-8') + ")")
 
 # Note: This code will save the availability data to a local file. In a real-world scenario, you might want to store this data more securely.
