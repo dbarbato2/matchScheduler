3035729fcbdb7d494320d8aa56c89143e56c106f
dbarbato2 <dbarbato2@gmail.com>
2023-10-17 15:33:57 -0400

    user availability v0.9

--- availability.py
+++ availability.py
@@ -1,5 +1,5 @@
 #Backlog items:
-# 1.Allow for open and close times to vary by day of the week
+# 1.Allow for open and close times to vary by day of the week - Done 10/17/2023
 # 2.Save the use availability files to a central location, i.e. S3 or Google Drive
 
 import streamlit as st
@@ -10,17 +10,25 @@ st. set_page_config(layout="wide")
 st.title("Availability Scheduler")
 
 # Create a form to collect user availability data
-playerName = st.text_area("Enter your name", height=1, max_chars=30)
+playerName = st.text_area("Enter your first and last name (i.e. 'John Smith')", height=1, max_chars=30)
 st.write("Check which times you are available for each day of the week:")
 
 days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
                 'Sunday']  # Define the start and end times
 colMonday, colTuesday, colWednesday, colThursday, colFriday, colSaturday, colSunday = st.columns(7)
 
-openTime = 8  # 8 AM
-closeTime = 21  # 9 PM
+openTime = (8, 8, 9, 8, 9, 6, 10)  # list for each day of the week, 8 = 8 AM
+closeTime = (21, 20, 21, 22, 21, 23, 20)  # list for each day of the week, 21 = 9 PM
+minOpenTime = min(openTime)
+st.write(minOpenTime)
+maxCloseTime = max(closeTime)
+st.write(maxCloseTime)
 
-availability_data = pd.DataFrame(columns=['day'])
+openHours = list(range(minOpenTime, maxCloseTime))
+st.write(openHours)
+openHours.insert(0, 'day')
+availability_data = pd.DataFrame(columns=[str(element) for element in openHours])
+st.write(availability_data)
 dayTracker = 1
 for day in days_of_week:
     if day == "Monday":
@@ -44,12 +52,15 @@ for day in days_of_week:
     else:
         with colSunday:
             st.subheader("Sunday")
-    #st.write("dayTracker = ", dayTracker)
-    #st.write("day = " + day)
     availability_data.loc[dayTracker-1, 'day'] = day
     hourTracker = 1
-    for hour in range(openTime, closeTime + 1):
+    for hour in range(minOpenTime, maxCloseTime):
+        closeInd = 0
         # Convert the hour to 12-hour format and determine AM or PM
+        if hour < openTime[dayTracker-1] or hour >= closeTime[dayTracker-1]:
+            closeInd = 1
+            availStr = False
+        origHour = hour # save the original 24 hour based number to call a specific column
         if hour < 12:
             am_pm = "AM"
             if hour == 0:
@@ -59,30 +70,28 @@ for day in days_of_week:
             if hour > 12:
                 hour -= 12  # Convert to 1-based 12-hour clock
         time_str = f"{hour:02d}:00 {am_pm}"
-        if day == "Monday":
+        if day == "Monday" and closeInd == 0:
             with colMonday:
                 availStr = st.checkbox(time_str, key=day + ", " + time_str)
-        elif day == "Tuesday":
+        elif day == "Tuesday" and closeInd == 0:
             with colTuesday:
                 availStr = st.checkbox(time_str, key=day + ", " + time_str)
-        elif day == "Wednesday":
+        elif day == "Wednesday" and closeInd == 0:
             with colWednesday:
                 availStr = st.checkbox(time_str, key=day + ", " + time_str)
-        elif day == "Thursday":
+        elif day == "Thursday" and closeInd == 0:
             with colThursday:
                 availStr = st.checkbox(time_str, key=day + ", " + time_str)
-        elif day == "Friday":
+        elif day == "Friday" and closeInd == 0:
             with colFriday:
                 availStr = st.checkbox(time_str, key=day + ", " + time_str)
-        elif day == "Saturday":
+        elif day == "Saturday" and closeInd == 0:
             with colSaturday:
                 availStr = st.checkbox(time_str, key=day + ", " + time_str)
-        else:
+        elif closeInd == 0:
             with colSunday:
                 availStr = st.checkbox(time_str, key=day + ", " + time_str)
-        #st.write("hourTracker = ", hourTracker)
-        #st.write("availStr = ", availStr)
-        availability_data.loc[dayTracker-1, hourTracker] = int(availStr == True)
+        availability_data.loc[dayTracker-1, str(origHour)] = int(availStr == True)
         hourTracker = hourTracker + 1
     dayTracker = dayTracker + 1
 
